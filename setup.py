2679888ddb9bce156f5c563ccf94c1488268df74
Christian Jurke <christian.jurke@gmail.com>
2016-01-24 16:59:27 +0100

    version bump: 0.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name="allthedots",
-      version="0.1.2",
+      version="0.1.3",
       author="Christian Jurke",
       description="Make a list of important stuff.",
 

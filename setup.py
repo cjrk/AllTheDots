00484e7dfb5763cd61821e3ab72ac3ec05557245
Christian Jurke <christian.jurke@gmail.com>
2016-01-24 18:16:22 +0100

    fix: dev: Fixed version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name="allthedots",
-      version="0.1.7",
+      version="0.1.8",
       author="Christian Jurke",
       author_email="christian.jurke@gmail.com",
       description="Make a list of important stuff.",
